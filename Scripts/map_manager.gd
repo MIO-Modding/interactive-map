90ed3167b1066b887374fe50b52d5a2ee1cb24c3
TheOmnipresence <ItsTheOmnipresence@gmail.com>
2026-08-30 12:18:01 -0400

    map settings

---
+++ Scripts/map_manager.gd
@@ -0,0 +1,12 @@
+extends Control
+
+
+@onready var map_node: Node2D = $SubViewportContainer/SubViewport/Node2D
+
+
+func _on_room_points_toggled(toggled_on: bool) -> void:
+	map_node.get_node("Points").visible = toggled_on
+
+
+func _on_transitions_toggled(toggled_on: bool) -> void:
+	pass # Replace with function body.
